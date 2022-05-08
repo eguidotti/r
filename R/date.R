3aa205cccdfed3751f0126f6e906e3226bf32f1c
Emanuele Guidotti <emanuele.guidotti@unine.ch>
2022-05-08 00:05:46 -0400

    Add as.Date

---
+++ R/date.R
@@ -0,0 +1,14 @@
+#' Convert YYYYMM to Date
+#' 
+#' @param x \code{integer} vector.
+#' @param day \code{character} specifying the day of the month.
+#' 
+#' @returns 
+#' vector of \code{Date}s
+#' 
+#' @export
+#' 
+as.Date.integer <- function(x, day = "01"){
+  as.Date(paste0(x, day), format = "%Y%m%d")  
+}
+  
