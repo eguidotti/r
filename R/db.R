2c7f40158245d33bb8bd5592eaceb28b668c3fb5
Emanuele Guidotti <emanuele.guidotti@unine.ch>
2023-04-09 00:30:00 +0200

    Clean code

--- R/db.R
+++ R/db.R
@@ -1,10 +1,12 @@
-#' Initialize DB
+#' Import tabular data into SQL
 #'
-#' Create a SQL database from flat files.
+#' Create SQL tables from \code{data.table}, \code{data.frame}, or flat files.
 #'
 #' @param conn a \code{DBIConnection} object, as returned by \code{\link[DBI]{dbConnect}}.
-#' @param path the path to the directory containing the data files.
-#' @param pattern an optional regular expression. Only file names which match the regular expression will be used.
+#' @param table \code{data.table}, \code{data.frame}, path to a tabular data file, or path to the directory containing tabular data files.
+#' @param name table name. Used only when \code{data} is a \code{data.table} or path to file.
+#' @param pattern an optional regular expression. Only file names into the \code{data} folder which match the regular expression will be used.
+#' @param index character vector of column names to use as index.
 #' @param overwrite if \code{TRUE}, an existing table of the same name will be overwritten. This argument doesn't change behavior if the table does not exist yet.
 #' @param verbose if \code{TRUE}, print on progress.
 #' @param na.strings a character vector of strings which are to be interpreted as \code{NA} values by \code{\link[data.table]{fread}}.
@@ -23,154 +25,157 @@
 #'
 #' @export
 #'
-dbInit <- function(conn, path, pattern = "\\.(rds|csv|gz)$", overwrite = FALSE, verbose = TRUE,  na.strings = "", ...) {
-  for(file in list.files(path = path, pattern = pattern, full.names = TRUE))
-    dbImportTable(conn, file = file, overwrite = overwrite, verbose = verbose, na.strings = na.strings, ...)
-}
-
-#' Export SQL Query to file
-#'
-#' @param conn a \code{DBIConnection} object, as returned by \code{\link[DBI]{dbConnect}}.
-#' @param sql the sql query or path to an .sql file.
-#' @param file output file name.
-#' @param verbose if \code{TRUE}, print on progress.
-#' @param ... additional arguments passed to \code{\link[data.table]{fwrite}}.
-#'
-#' @returns
-#' \code{NULL}
-#'
-#' @export
-#'
-dbExport <- function(conn, sql, file, verbose = TRUE, ...){
-  if(verbose)
-    cat("Running query... ")
+dbImport <- function(
+    conn, 
+    data, 
+    name = NULL,
+    pattern = "\\.(rds|csv|gz)$", 
+    index = NULL,
+    overwrite = FALSE, 
+    verbose = TRUE,  
+    na.strings = "", 
+    ...) {
   
-  if(endsWith(sql, ".sql"))
-    sql <- readChar(sql, file.info(sql)$size)
-  x <- dbGetQuery(conn, sql)
+  if(is.character(data) && dir.exists(data)){
 
-  if(verbose)
-    cat("writing file... ")
-  fwrite(x, file = file, showProgress = verbose, ...)
+    for(file in list.files(path = data, pattern = pattern, full.names = TRUE)){
+      
+      dbImportTable(
+        conn, 
+        table = file, 
+        name = gsub("\\..*$", "", basename(file)),
+        index = index,
+        overwrite = overwrite, 
+        verbose = verbose, 
+        na.strings = na.strings, 
+        ...
+      )
+      
+    }
 
-  if(verbose)
-    cat(sprintf("\n\nFile saved in %s\n", file))
+  }
+  
+  else {
+    
+    dbImportTable(
+      conn, 
+      table = table, 
+      name = name,
+      index = index,
+      overwrite = overwrite, 
+      verbose = verbose, 
+      na.strings = na.strings, 
+      ...
+    )
+    
+  }
+
 }
 
 #' Import Table
 #'
-#' @param conn a \code{DBIConnection} object, as returned by \code{\link[DBI]{dbConnect}}.
-#' @param file the file to import.
+#' @param conn \code{DBIConnection} object, as returned by \code{\link[DBI]{dbConnect}}.
+#' @param table \code{data.table}, \code{data.frame}, or path to a data file.
+#' @param name the table name.
+#' @param index character vector of column names to use as index.
 #' @param overwrite if \code{TRUE}, an existing table of the same name will be overwritten. This argument doesn't change behavior if the table does not exist yet.
 #' @param verbose if \code{TRUE}, print on progress.
-#' @param na.strings a character vector of strings which are to be interpreted as \code{NA} values by \code{\link[data.table]{fread}}.
+#' @param na.strings character vector of strings which are to be interpreted as \code{NA} values by \code{\link[data.table]{fread}}.
 #' @param ... additional arguments passed to \code{\link[data.table]{fread}}.
 #'
 #' @returns
 #' \code{NULL}
 #'
-dbImportTable <- function(conn, file, overwrite, verbose, na.strings, ...) {
-  if(verbose)
-    cat(sprintf("Import: %s\n", file))
-
-  name <- gsub("\\..*$", "", basename(file))
-  exists <- dbExistsTable(conn, name = name)
-  if(exists & !overwrite){
-    if(verbose)
-      cat(sprintf("  -> skipped. Table %s already exists and overwrite=FALSE.\n", name))
-    return(invisible())
-  }
-
-  if(verbose)
-    cat("  ->  reading file...\n")
-  if(endsWith(file, ".rds"))
-    x <- readRDS(file)
-  else
-    x <- fread(file = file, na.strings = na.strings, showProgress = FALSE, ...)
-
-  if(verbose)
-    cat("  ->  sanitizing data...\n")
-  x <- dbSanitizeData(x)
-
-  if(verbose)
-    cat(sprintf("  ->  %s table %s...\n", ifelse(exists, "overwriting", "creating"), name))
-  dbWriteTable(conn, name = name, value = x, overwrite = overwrite, row.names = FALSE)
-
-  if(verbose)
-    cat("  ->  creating index...\n")
-  dbCreateIndex(conn, name)
-
-  if(verbose)
-    cat("  ->  done!\n")
-}
-
-#' Create Index
-#'
-#' @param conn a \code{DBIConnection} object, as returned by \code{\link[DBI]{dbConnect}}.
-#' @param name the table name.
-#' @param index vector of column names to index the table.
-#'
-#' @returns
-#' \code{NULL}
-#' 
-#' @export
-#'
-dbCreateIndex <- function(conn, name, index = NULL){
-  pk <- c(
-    "sfz_del" = "KYPERMNO, DLSTDT",
-    "sfz_mdel" = "KYPERMNO, MDLSTDT",
-    "sfz_dis" = "KYPERMNO, EXDT, DISTCD, ACPERM",
-    "sfz_nam" = "KYPERMNO, NAMEDT",
-    "sfz_ndi" = "KYPERMNO, TRTSDT",
-    "sfz_hdr" = "KYPERMNO",
-    "sfz_shr" = "KYPERMNO, SHRSDT",
-    "sfz_dp_dly" = "KYPERMNO, CALDT",
-    "sfz_ds_dly" = "KYPERMNO, CALDT",
-    "sfz_mth" = "KYPERMNO, MCALDT",
-    "sfz_agg_mth" = "KYPERMNO, YYYYMM",
-    "sfz_agg_qtr" = "KYPERMNO, YYYYQ",
-    "sfz_agg_ann" = "KYPERMNO, YYYY",
-    "sfz_mbr" = "KYPERMNO, KEYSET, MBRDT",
-    "sfz_portd" = "KYPERMNO, KEYSET, ANNUAL",
-    "sfz_portm" = "KYPERMNO, KEYSET, ANNUAL",
-    "sfz_indhdr" = "KYINDNO",
-    "sfz_rb" = "KYINDNO, RBBEGDT",
-    "sfz_dind" = "KYINDNO, CALDT",
-    "sfz_mind" = "KYINDNO, MCALDT"
-  )
-
-  if(!is.null(index))
-    cols <- paste(index, collapse = ", ")
-  else if(name %in% names(pk))
-    cols <- pk[name]
-  else
-    return(invisible())
+dbImportTable <- function(
+    conn, 
+    table, 
+    name,
+    index,
+    overwrite, 
+    verbose, 
+    na.strings, 
+    ...) {
   
-  if(dbExistsTable(conn, name = name)){
-    sql <- sprintf("CREATE INDEX idx_%s ON %s (%s)", name, name, cols)
-    dbExecute(conn, sql)  
+  if(verbose) cat(sprintf("Import table: %s\n", name))
+
+  if(dbExistsTable(conn, name = name) & !overwrite){
+    
+    if(verbose) cat(sprintf(
+      "  -> skipped. Table %s already exists and overwrite=FALSE.\n", 
+      name
+    ))
+    
   }
   
-  return(invisible())
-}
+  else{
+    
+    if(is.character(table)){
+      
+      if(verbose) cat("  ->  reading file...\n")
+      
+      if(endsWith(table, ".rds"))
+        table <- readRDS(table)
+      
+      else
+        table <- fread(
+          file = table, 
+          na.strings = na.strings, 
+          showProgress = FALSE, 
+          ...)
+      
+    }
+    
+    else if (is.data.table(table) & is.null(index)){
+      
+      index <- key(table)
+      
+    }
+    
+    if(verbose) cat("  ->  sanitizing data...\n")
+    
+    table <- as.data.frame(table)
+    colnames(table) <- tolower(colnames(table))
+    
+    for(i in colnames(table))
+      if(inherits(table[,i], 'Date'))
+        table[,i] <- as.integer(format(table[,i], format = "%Y%m%d"))
+    
+    if("itemlongdesc" %in% colnames(table))
+      table$itemlongdesc <- gsub("[\x80-\xff]", "", table$itemlongdesc)
+    
+    if(verbose) cat(sprintf(
+      "  ->  %s table %s...\n", 
+      ifelse(exists, "overwriting", "creating"), 
+      name
+    ))
+    
+    dbWriteTable(
+      conn, 
+      name = name, 
+      value = table, 
+      overwrite = overwrite, 
+      row.names = FALSE
+    )
+    
+    if(!is.null(index)){
+      
+      cols <- paste(intersect(index, colnames(table)), collapse = ", ")
+      
+      if(cols != ""){
+        
+        if(verbose) cat("  ->  creating index...\n")
+        
+        dbExecute(conn, sprintf(
+          "CREATE INDEX idx_%s ON %s (%s)", 
+          name, name, cols
+        ))  
+        
+      }
+      
+    }
+    
+    if(verbose) cat("  ->  done!\n")
+    
+  }
 
-#' Sanitize Data
-#'
-#' @param x a data frame.
-#'
-#' @returns
-#' \code{data.frame}
-#'
-dbSanitizeData <- function(x){
-  x <- as.data.frame(x)
-  colnames(x) <- tolower(colnames(x))
-
-  for(i in colnames(x))
-    if(inherits(x[,i], 'Date'))
-      x[,i] <- as.integer(format(x[,i], format="%Y%m%d"))
-
-  if("itemlongdesc" %in% colnames(x))
-    x$itemlongdesc <- gsub("[\x80-\xff]", "", x$itemlongdesc)
-
-  return(x)
 }
